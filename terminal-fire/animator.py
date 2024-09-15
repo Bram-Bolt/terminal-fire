c585ba840fd36b8a2793432da193072d74c56ecc
Bram Bolt <bram.bolt@icloud.com>
2024-09-15 22:07:20 +0200

    added documentation

--- terminal-fire/animator.py
+++ terminal-fire/animator.py
@@ -4,7 +4,9 @@ import time
 
 
 def animate_sequence(seconds, height, width, config):
+    """Updates frames for a certain amount of seconds"""
     canvas = Frame(height, width, config)
+    # convert frames to seconds
     n_frames = seconds * 10
     for i in range(n_frames):
         # TODO: make dynamic?
@@ -13,6 +15,7 @@ def animate_sequence(seconds, height, width, config):
         #     canvas = Frame(height, width, config)
         #     height, width = new_h, new_w
         print(canvas)
+        # print(f"frame: {i}")  # debug
         canvas.shift_frame()
         canvas.age_frame()
         time.sleep(0.1)
--- terminal-fire/frame.py
+++ terminal-fire/frame.py
@@ -5,24 +5,29 @@ import random
 from colour_config import ANSI
 
 
+# TODO need better name for "age", since it goes down. "lives" sounds a bit weird?
 class Frame:
     def __init__(self, height, width, config):
         self.frame = np.zeros((height, width), dtype=int)
 
         # TODO STEP A WAY FROM HARDCODING!!! SHOULD BE DYNAMIC BASED ON LENGTH
+        self.scale_factor = 0.7  # scaling factor for treshold
+
         self.mean = 0.5
         self.std_dev = 0.5
-        self.scale_factor = 0.7
-
         self.normal_dist_array = generate_normal_array(self.mean, width, self.std_dev)
-        self.colour_encoding, self.seq_encoding = config
-        self.max_colour = len(self.seq_encoding) - 1
+
+        self.colour_encoding, self.seq_encoding = config  # sets encoding
+        self.starting_age = len(self.seq_encoding) - 1  # sets starting char age
 
     def shift_frame(self):
+        """Shift the ids in the frame upwards, resetting the new bottom row"""
         self.frame = np.roll(self.frame, shift=-1, axis=0)
-        self.frame[-1, :] = self.max_colour
+        self.frame[-1, :] = self.starting_age
 
     def age_frame(self):
+        """Age alls items in frame based on probabilities and the distribution array."""
+
         # Generate random probabilities
         random_probs = np.random.rand(*self.frame.shape)
 
@@ -38,12 +43,15 @@ class Frame:
         # Apply -1 age on items where both masks hold
         self.frame = np.where(non_zero_mask & aged_mask, self.frame - 1, self.frame)
 
-    def get_coloured_char(self, i):
-        c_id = int(self.seq_encoding[i])
+    def get_coloured_char(self, age):
+        """Return the ANSI colored character for the given char age"""
+        c_id = int(self.seq_encoding[age])
         colour, c = self.colour_encoding[c_id]
         return f"{colour.value}{c}{ANSI.RESET.value}"
 
     def __str__(self):
+        """Convert the frame to a string with colored characters."""
+
         # Vectors are similar to maps, but are used on np.arrays.
         # Vectorisation changes the elements from their id to a coloured string representation
         vectorized_coloured_char = np.vectorize(self.get_coloured_char)
