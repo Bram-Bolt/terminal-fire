175b37148f8a5dfe082dd724b179da03aa903f3f
Bram Bolt <bram.bolt@icloud.com>
2024-09-15 22:11:47 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terminal-fire",
+    version="0.0.3",
+    description="A terminal-based fire animation using numpy",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Bram Bolt",
+    author_email="contact@brambolt.me",
+    url="https://github.com/bram-bolt/terminal-fire",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "terminal-fire=terminal_fire.main:main",
+        ],
+    },
+)
