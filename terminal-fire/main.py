c069bace5ce2d63864b097cdecfc6dbf5d525f84
Bram Bolt <bram.bolt@icloud.com>
2024-09-15 18:50:54 +0200

    added automatic sizing at startup

--- terminal-fire/main.py
+++ terminal-fire/main.py
@@ -1,8 +1,9 @@
 from animator import animate_sequence
 from colour_config import colour_encoding, seq_encoding
+import os
 
-h = 40
-w = 100
+
+w, h = os.get_terminal_size()
 
 config = (colour_encoding, seq_encoding)
 animate_sequence(10, h, w, config)
