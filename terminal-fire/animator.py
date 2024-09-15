2df65c53a55de2effd6365c100921ef11ca59b6f
Bram Bolt <bram.bolt@icloud.com>
2024-09-15 21:49:10 +0200

    added animator concept

--- terminal-fire/animator.py
+++ terminal-fire/animator.py
@@ -7,6 +7,11 @@ def animate_sequence(seconds, height, width, config):
     canvas = Frame(height, width, config)
     n_frames = seconds * 10
     for i in range(n_frames):
+        # TODO: make dynamic?
+        # new_w, new_h = os.get_terminal_size()
+        # if (height, width) != (new_h, new_w):
+        #     canvas = Frame(height, width, config)
+        #     height, width = new_h, new_w
         print(canvas)
         canvas.shift_frame()
         canvas.age_frame()
